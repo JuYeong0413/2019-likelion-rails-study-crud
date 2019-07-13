d5f5be1ca8b2a003e44efd1e25dbca66af803300
JuYeong0413 <2jy22@naver.com>
2019-07-14 02:02:00 +0900

    crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,46 @@
 class PostsController < ApplicationController
+    before_action :load_post, only: [:show, :edit, :update, :destroy]
+    
     def index
+        @posts = Post.all
     end
-
+    
+    # 글쓰기
     def new
+        @post = Post.new
     end
-
+    
     def create
+        @post = Post.new(set_params)
+        @post.save
+        redirect_to root_path
     end
-
+    
+    # 상세보기
+    def show
+    end
+    
+    # 수정
     def edit
     end
-
+    
     def update
+        @post.update(set_params)
+        redirect_to post_path(@post)
     end
-
-    def delete
+    
+    # 삭제
+    def destroy
+        @post.destroy
+        redirect_to root_path
+    end
+    
+    private
+    def load_post
+        @post = Post.find params[:id]
+    end
+    
+    def set_params
+        params.require(:post).permit(:title, :writer, :content)
     end
 end
